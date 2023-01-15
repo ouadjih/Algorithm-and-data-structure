6d9b28d5494cadde4d409de50a60da8ca9e43c22
ouadjih <47463757+ouadjih@users.noreply.github.com>
2023-01-15 21:46:59 +0100

    added serie05 solution

---
+++ serie5/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main()
+{
+    //Exercice1: la presence d'un caractère dans une chaine
+    /*
+    char x = 'a';
+    char chaine[200]="c'est une chaine de caracetere";
+    for (int i = 0; i<31; i++){
+        if(x == chaine[i]){ printf("le caractere existe"); return 0;}
+    }
+    printf("le caractère n'existe pas !");
+    return 0;*/
+    //Exercice 2:
+
+}
