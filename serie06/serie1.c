42daf1afbd9e5b9c2666eeca035a0ad94d13003a
ouadjih <47463757+ouadjih@users.noreply.github.com>
2023-03-05 14:28:00 +0100

    added exercice 5 and 6 solution

--- serie06/serie1.c
+++ serie06/serie1.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #include<stdbool.h>
+#include<string.h>
 
 int Factorielle(int N)
  {
@@ -8,6 +9,7 @@ int Factorielle(int N)
     fact = fact * i ;
     return fact;
  }
+
 int maximum(int a ,int b){
  if(a<b){
         return b ;
@@ -23,10 +25,34 @@ bool deviser(int a ,int b){
         else
         return false;
 }
+
 int ABS(int a){
    return a<0?-a:a;
 }
+
+void isPalindrome(const char * mot){
+    int L = strlen(mot);
+    int i,j;
+    for(i=0,j=L-1;i<j;i++,j--){
+        if(mot[i]!=mot[j])
+        {
+            printf("Le mot n'est pas palindrom");
+            break;
+        }
+    }
+    if(i>=j)
+    printf("Le mot est palindrom");
+}
+
+int isMultipleDeux(int a){
+return a%2 == 0 ? 1:0;
+}
+int isMultipleTrois(int a){
+return a%3 == 0?1:0;
+}
+
  int main(){
+
     /* Exercice 01
 
     int x,y,z;
@@ -49,7 +75,7 @@ int ABS(int a){
     /*
     Exercice 3
 
-    */
+
     int tab[10]={1, 22, -32, 45, 0, 5, 66, -73, 21, 99},max=tab[0],sum;
 
     for (int i = 0; i<=9;i++){
@@ -59,6 +85,25 @@ int ABS(int a){
         printf("le maximum d'un tableau : %d ",max);
         printf("\nla somme des val absolue de tableau : %d ",sum);
 
+    */
+    /* Exercice 5: verifie si un mot est palindrome
 
+     char mot[100];
 
+    printf("Entrer le mot : ");
+    scanf("%s", mot);
+
+    isPalindrome(mot);
+    */
+
+    /*Exercice 6:
+
+    */
+    int x;
+    printf("donner x : ");
+    scanf("%d",&x);
+
+    if(isMultipleDeux(x)) printf("%d est pair\n",x);
+    if(isMultipleTrois(x)) printf("%d est multiple de 3\n",x);
+    if(isMultipleDeux(x) && isMultipleTrois(x)) printf("%d est divisible de 6",x);
 return 0;}
