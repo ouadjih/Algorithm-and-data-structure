1f3b9951729ebc5b1f9b0fd36b705bf0dfeef2ce
ouadjih <47463757+ouadjih@users.noreply.github.com>
2023-03-12 13:19:10 +0100

    added correction of exercice 1 and exercice 2

--- serie07/enregistrement.c
+++ serie07/enregistrement.c
@@ -1,12 +1,23 @@
 #include<stdio.h>
+#include<string.h>
 
-typedef struct complexe {
+typedef struct nombreComplexe
+{
     int reelle;
     int imaginaire;
-}complexe;
+} complexe;
 
-int main(){
-    /*Exercice01 : */
+typedef struct Etudiant
+{
+    char nom[30];
+    char prenom[30];
+    int unsigned age;
+    float moyenne;
+} Etudiant;
+
+int main()
+{
+    /*Exercice01 :
     complexe z1,z2,somme_complexe,produit_complexe;
 
     printf("Donner la partie reelle pui l'imaginaire de z1 : ");
@@ -17,8 +28,8 @@ int main(){
     printf("\nz1 = %d+%di",z1.reelle,z1.imaginaire);
     printf("\nz2 = %d+%di",z2.reelle,z2.imaginaire);
 
-   somme_complexe.reelle= z1.reelle+z2.reelle;
-   somme_complexe.imaginaire= z1.imaginaire+z2.imaginaire;
+    somme_complexe.reelle= z1.reelle+z2.reelle;
+    somme_complexe.imaginaire= z1.imaginaire+z2.imaginaire;
 
     printf("\nz1+z2 = %d + %di",somme_complexe.reelle,somme_complexe.imaginaire);
 
@@ -27,5 +38,40 @@ int main(){
 
     printf("\nz1*z2 = %d + %di",produit_complexe.reelle,produit_complexe.imaginaire);
 
+     */
+
+    Etudiant etudiant[100];
+
+    for(int i = 0; i<2; i++)
+    {
+        printf("\nEtudiant %d : ",i+1);
+        printf("\n  nom : ");
+        scanf("%s",&etudiant[i].nom);
+        printf("  prenom : ");
+        scanf("%s",&etudiant[i].prenom);
+        printf("  age : ");
+        scanf("%d",&etudiant[i].age);
+        printf("  Moyenne : ");
+        scanf("%f",&etudiant[i].moyenne);
+    }
+    printf("------- Affichage de resultat pour chaque d'etudiant -------");
+    for(int i = 0; i<2; i++)
+    {
+        printf("\nEtudiant %d : ",i+1);
+        printf("\n  nom : %s",etudiant[i].nom);
+        printf("\n  prenom : %s",etudiant[i].prenom);
+        printf("\n  age : %d",etudiant[i].age);
+        printf("\n  Moyenne : %f",etudiant[i].moyenne);
+        if(etudiant[i].moyenne>=10)
+        {
+            printf("ADMIS");
+        }
+        else
+        {
+            printf("AJOURNE");
+        }
+    }
+
+    return 0;
 
 }
